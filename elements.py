9893d9f4e719ee4a6838692f59bc354c63e3981e
gusaaaaa <gusa@neo.com>
2013-01-28 18:51:22 -0200

    Adds icon, start and pause functions

--- elements.py
+++ elements.py
@@ -15,10 +15,12 @@ class CurrentEntry(Subject):
     signal.signal(signal.SIGALRM, self.__pool_server)
     signal.setitimer(signal.ITIMER_REAL, pooling_interval, pooling_interval)
     
-  # def start_timer:
+  def start_timer(self):
+    self.__api.start_current_entry_timer()
 
-  # def stop_timer:
-
+  def pause_timer(self):
+    self.__api.pause_current_entry_timer(self.data['id'])
+  
   # def log:
 
   def __pool_server(self, signum, frame):
--- minutedock.py
+++ minutedock.py
@@ -18,24 +18,57 @@ from elements import CurrentEntry
 from datetime import datetime, timedelta
 
 class MinutedockUbuntu:
-  
+
   def __init__(self):
-    current_entry = CurrentEntry(5)
-    self.timer = timedelta(seconds=int(current_entry.data['duration']))
-    self.datetime = datetime(1, 1, 1) + self.timer
+    self.current_entry = CurrentEntry(5)
+    self.current_entry.attach(self)
+    self.data = self.current_entry.data
 
-    current_entry.attach(self)
+    timer = timedelta(seconds=int(self.data['duration']))
+    clock = datetime(1, 1, 1) + timer
 
-    self.ind = appindicator.Indicator ("minutedock-ubuntu", 
+    self.ind = appindicator.Indicator("minutedock-ubuntu", 
       "indicator-messages", 
       appindicator.CATEGORY_APPLICATION_STATUS)
-    self.ind.set_status (appindicator.STATUS_ACTIVE)
-    self.ind.set_attention_icon ("indicator-messages-new")
-    self.ind.set_icon("distributor-logo")
-    self.ind.set_label("MinuteDock - {0}:{1}".format(self.datetime.hour, self.datetime.minute))
+    
+    self.ind.set_status(appindicator.STATUS_ACTIVE)
+    self.ind.set_attention_icon("indicator-messages-new")
+    self.ind.set_icon_theme_path(os.path.realpath('.'))
+    self.ind.set_icon("minutedock")
+    self.ind.set_label("%02d:%02d" % (clock.hour, clock.minute))
 
     # create a menu
     self.menu = gtk.Menu()
+
+    self.update_menu()
+
+    self.ind.set_menu(self.menu)
+
+  def update(self, data):
+    self.data = data
+    timer = timedelta(seconds=int(data['duration']))
+    clock = datetime(1, 1, 1) + timer
+    self.ind.set_label("%02d:%02d" % (clock.hour, clock.minute))
+    self.update_menu()
+
+  def update_menu(self):
+    self.menu.hide()
+
+    for i in self.menu.get_children():
+      self.menu.remove(i)
+    
+    # create items for the menu - labels, checkboxes, radio buttons and images are supported:
+
+    if self.data['timer_active']:
+      item = gtk.MenuItem("Pause")
+      item.show()
+      self.menu.append(item)
+    else:
+      item = gtk.MenuItem("Start")
+      item.show()
+      self.menu.append(item)
+
+    item.connect("activate", self.toggle_timer, self.data['timer_active'])
 
     image = gtk.ImageMenuItem(gtk.STOCK_QUIT)
     image.connect("activate", self.quit)
@@ -44,16 +77,14 @@ class MinutedockUbuntu:
 
     self.menu.show()
 
-    self.ind.set_menu(self.menu)
-
-  def update(self, data):
-    self.timer = timedelta(seconds=int(data['duration']))
-    self.datetime = datetime(1, 1, 1) + self.timer
-    self.ind.set_label("MinuteDock - {0}:{1}".format(self.datetime.hour, self.datetime.minute))
-
   def quit(self, widget, data=None):
     gtk.main_quit()
 
+  def toggle_timer(self, widget, timer_active):
+    if timer_active:
+      self.current_entry.pause_timer()
+    else:
+      self.current_entry.start_timer()
 
 def main():
   gtk.main()
