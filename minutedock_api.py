8b52f73b93771d21d5935b750029300403232d38
gusaaaaa <gusa@neo.com>
2013-01-28 18:51:22 -0200

    Adds icon, start and pause functions

--- minutedock_api.py
+++ minutedock_api.py
@@ -1,4 +1,5 @@
 import urllib
+import urllib2
 import json
 import os
 
@@ -19,10 +20,10 @@ class MinutedockApi:
     return json.loads(response)
 
   def start_current_entry_timer(self):
-    response = urllib.urlopen(self.URL_BASE + '/entries/current/start.json?api_key=' + self.api_key)
+    response = urllib2.urlopen(self.URL_BASE + '/entries/current/start.json?api_key=' + self.api_key, "").read()
 
-  def pause_current_entry_timer(self):
-    response = urllib.urlopen(self.URL_BASE + '/entries/current/pause.json?api_key=' + self.api_key)
+  def pause_current_entry_timer(self, entry_id):
+    response = urllib2.urlopen(self.URL_BASE + "/entries/%s/pause.json?api_key=%s" % (entry_id, self.api_key), "").read()
 
   def log_current_entry(self):
     response = urllib.urlopen(self.URL_BASE + '/entries/current/log.json?api_key=' + self.api_key)
